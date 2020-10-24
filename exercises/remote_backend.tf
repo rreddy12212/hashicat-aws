31ed3bff614d00d4915652a2c1747176c78b443f
Ram Panyala <rreddy12212@gmail.com>
2020-10-24 07:22:22 +0000

    adding remote backend

--- exercises/remote_backend.tf
+++ exercises/remote_backend.tf
@@ -5,9 +5,9 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "YOURORGANIZATION"
+    organization = "rampanyala-training"
     workspaces {
-      name = "YOURWORKSPACE"
+      name =  "hashicat-aws"
     }
   }
 }
